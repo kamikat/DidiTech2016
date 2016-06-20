c7d075a1376e790712467ebbc5f0015e96eedfda
kirisetsz <kirisetsz@gmail.com>
2016-06-21 00:19:20 +0800

    update date

--- src/order-transformer.py
+++ src/order-transformer.py
@@ -9,9 +9,11 @@ def nullable_string(data):
         return '"%s"' % data
 
 with open(sys.argv[1], "r") as data:
-    print 'INSERT INTO didi.`order` (order_id, driver_id, passenger_id, start_district_hash, dest_district_hash, price, date, time_slot) VALUES '
     line = data.readline()
+    count = 0;
     while line:
+        if count == 0:
+            print 'INSERT INTO didi.`order` (order_id, driver_id, passenger_id, start_district_hash, dest_district_hash, price, date, time_slot) VALUES',
         (order_id, driver_id, passenger_id, start_district_hash, dest_district_hash, price, datetime) = line.split('\t')
         datetime = datetime.split(" ")
         time_slot = datetime[1].split(":")
@@ -26,6 +28,11 @@ with open(sys.argv[1], "r") as data:
                 price,
                 nullable_string(datetime[0]),
                 time_slot),
+        count += 1
+        if count == 100:
+            print 'ON DUPLICATE KEY UPDATE driver_id = driver_id;'
+            count = 0
+            continue
         if line:
-            print ','
-    print ';'
+            print ',',
+    print ' ON DUPLICATE KEY UPDATE driver_id = driver_id;'
