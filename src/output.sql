3f784522a25c159d067e752552ceecf8e358bd03
kirisetsz <kirisetsz@gmail.com>
2016-06-21 15:54:05 +0800

    fix output

--- src/output.sql
+++ src/output.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT("\"", district_hash, "\",\"", date, "\",", time_slot, ",", is_holiday, ",", weather, ",", temperature, ",", pm25, ",", level_1, ",", level_2, ",", level_3, ",", level_4, poi.poi_feature, gap)
+CONCAT(district_hash,",",date,",",time_slot,",",is_holiday,",",weather,",",temperature,",",pm25,",",level_1,",",level_2,",",level_3,",",level_4,",",poi.poi_feature,",",gap)
 FROM intermediate
 JOIN (
   SELECT
