9670987724db3763c1c5aadaec98d361e7bd809e
kirisetsz <kirisetsz@gmail.com>
2016-06-22 09:18:05 +0800

    update forest

--- src/randomForest.py
+++ src/randomForest.py
@@ -2,6 +2,7 @@
 
 import sys
 import numpy as np
+from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble.forest import RandomForestRegressor
 
@@ -34,15 +35,10 @@ is_train = np.random.uniform(0, 1, len(dataset)) <= .75
 training, testingSet = dataset[is_train], dataset[~is_train]
 
 # train model
-model = RandomForestRegressor(n_estimators=int(sys.argv[2]), n_jobs=-1)
-model.fit(training[:,:-1], training[:,-1])
-
-# importance vector
-print zip([
-    "district_hash","date","time_slot","is_holiday",
-    "weather","temperature","pm25",
-    "level_1","level_2","level_3","level_4"] +
-    [ "poi_%d" % x for x in range(0, 25) ], model.feature_importances_)
+def train(k):
+    model = RandomForestRegressor(n_estimators=k, n_jobs=-1, verbose=1)
+    model.fit(training[:,:-1], training[:,-1])
+    return model
 
 def predict(model, testing):
     prediction = model.predict(testing[:,:-1])
@@ -51,6 +47,17 @@ def predict(model, testing):
     validation = validation[np.nonzero(validation)]
     return np.average(np.abs(prediction - validation) / validation)
 
-for i in xrange(0, 5):
-    print predict(model, testingSet[np.random.uniform(0, 1, len(testingSet)) <= .5])
+def perform(model):
+    # importance vector
+    print "Importances:", zip([
+        "district_hash","date","time_slot","is_holiday",
+        "weather","temperature","pm25",
+        "level_1","level_2","level_3","level_4"] +
+        [ "poi_%d" % x for x in range(0, 25) ], model.feature_importances_)
+    print "MAPS =", predict(model, testingSet)
+    print
+
+perform(train(10))
+perform(train(100))
+perform(train(1000))
 
