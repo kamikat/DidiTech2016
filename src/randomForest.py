b50b5aeed4db20adcf2842b7c8c3e3b5df0a52d6
kirisetsz <kirisetsz@gmail.com>
2016-06-22 21:28:55 +0800

    update data format

--- src/randomForest.py
+++ src/randomForest.py
@@ -11,24 +11,19 @@ dataset_ = []
 with open(sys.argv[1]) as data:
     line = data.readline()
     while line:
-        vec = line.split(",")
+        vec = line.strip().split("\t")
         dataset_.append(tuple(vec[:]))
         line = data.readline()
 
-dataset = np.asarray(dataset_)
+columns = dataset_[0]
+dataset = np.asarray(dataset_[1:])
 
 district = LabelEncoder()
 date = LabelEncoder()
-time_slot = LabelEncoder()
-is_holiday = LabelEncoder()
-weather = LabelEncoder()
 
 dataset[:,0] = district.fit_transform(dataset[:,0])
 dataset[:,1] = date.fit_transform(dataset[:,1])
-dataset[:,2] = time_slot.fit_transform(dataset[:,2])
-dataset[:,3] = is_holiday.fit_transform(dataset[:,3])
-dataset[:,4] = weather.fit_transform(dataset[:,4])
-dataset[:,5:] = dataset[:,5:].astype(np.float)
+dataset[:,2:] = dataset[:,2:].astype(np.float)
 
 # split data set
 def split(dataset):
@@ -53,11 +48,7 @@ def perform(dataset, k):
     training, testing = split(dataset)
     model = train(training, k)
     # importance vector
-    print >>sys.stderr, "Importance:", dict(zip([
-        "district_hash","date","time_slot","is_holiday",
-        "weather","temperature","pm25",
-        "level_1","level_2","level_3","level_4"] +
-        [ "poi_%d" % x for x in range(0, 25) ], model.feature_importances_))
+    print >>sys.stderr, "Importance:", zip(columns[:-1], model.feature_importances_)
     maps = predict(model, testing)
     print >>sys.stderr, "MAPS:", maps
     # tree.export_graphviz(model.estimators_[0].tree_)
@@ -70,6 +61,7 @@ def measure(district_id, k):
     avg = np.average([ perform(dataset[dataset[:,0].astype(int) == district_id], k) for i in xrange(1, 10) ])
     print >>sys.stderr, "-" * 42
     print >>sys.stderr, "AVG(MAPS[%s]) =" % district.inverse_transform(district_id), avg
+    print
     return avg
 
 print np.average([ measure(district_id, int(sys.argv[2])) for district_id in np.unique(dataset[:,0]).astype(int) ])
