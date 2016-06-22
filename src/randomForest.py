e471209d2f3574fe0b04b7a16db848ded35d32c4
kirisetsz <kirisetsz@gmail.com>
2016-06-22 17:34:43 +0800

    aggregate result in per-district basis

--- src/randomForest.py
+++ src/randomForest.py
@@ -31,15 +31,17 @@ dataset[:,4] = weather.fit_transform(dataset[:,4])
 dataset[:,5:] = dataset[:,5:].astype(np.float)
 
 # split data set
-is_train = np.random.uniform(0, 1, len(dataset)) <= .75
-training, testingSet = dataset[is_train], dataset[~is_train]
+def split(dataset):
+    is_train = np.random.uniform(0, 1, len(dataset)) <= .75
+    return dataset[is_train], dataset[~is_train]
 
 # train model
-def train(k):
-    model = RandomForestRegressor(n_estimators=k, n_jobs=-1, verbose=1)
+def train(training, k):
+    model = RandomForestRegressor(n_estimators=k, n_jobs=-1)
     model.fit(training[:,:-1], training[:,-1])
     return model
 
+# predict and evaluate MAPE value
 def predict(model, testing):
     prediction = model.predict(testing[:,:-1])
     validation = np.array(testing[:,-1], dtype=np.float)
@@ -47,18 +49,25 @@ def predict(model, testing):
     validation = validation[np.nonzero(validation)]
     return np.average(np.abs(prediction - validation) / validation)
 
-def perform(model):
+def perform(dataset, k):
+    training, testing = split(dataset)
+    model = train(training, k)
     # importance vector
-    print "Importances:", zip([
+    print >>sys.stderr, "Importances:", zip([
         "district_hash","date","time_slot","is_holiday",
         "weather","temperature","pm25",
         "level_1","level_2","level_3","level_4"] +
         [ "poi_%d" % x for x in range(0, 25) ], model.feature_importances_)
-    print "MAPS =", predict(model, testingSet)
-    print
-    tree.export_graphviz(model.estimators_[0].tree_)
+    maps = predict(model, testing)
+    print >>sys.stderr, "MAPS =", maps
+    # tree.export_graphviz(model.estimators_[0].tree_)
+    return maps
 
-perform(train(10))
-perform(train(100))
-perform(train(1000))
+def measure(indexing, k):
+    return np.average([ perform(dataset[indexing], k) for i in xrange(1, 10) ])
+
+for district_id in np.unique(dataset[:,0]).astype(int):
+    print "%s: %f" % (
+            district.inverse_transform(district_id),
+            measure(dataset[:,0].astype(int) == district_id, 32))
 
