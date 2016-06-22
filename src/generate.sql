b79922772e703422fbbb9aaab2e3d6f828412954
kirisetsz <kirisetsz@gmail.com>
2016-06-22 20:40:17 +0800

    update generate & process

--- src/generate.sql
+++ src/generate.sql
@@ -1,9 +1,15 @@
 -- generate training data over gap
 SELECT
-  prediction.district                     AS district,
-  prediction.date                         AS date,
+  prediction.district                   AS district,
+  prediction.date                       AS date,
   features.is_holiday                   AS is_holiday,
-  features.weather                      AS weather,
+  features.weather = 1                  AS weather_1,
+  features.weather = 2                  AS weather_2,
+  features.weather = 3                  AS weather_3,
+  features.weather = 4                  AS weather_4,
+  features.weather = 5                  AS weather_5,
+  features.weather = 6                  AS weather_6,
+  features.weather = 7                  AS weather_7,
   features.temperature                  AS temperature,
   features.pm25                         AS pm25,
   features.level_1                      AS level_1,
--- src/process.sql
+++ src/process.sql
@@ -1,71 +1,57 @@
 -- aggregate demand answer table on each time slot
+CREATE INDEX order_start_district_hash_date_time_slot_index ON `order` (start_district_hash, date, time_slot);
+
+DROP TABLE IF EXISTS order_stat;
+
 CREATE TABLE IF NOT EXISTS order_stat (
   SELECT
-    start_district_hash        AS district_hash,
+    start_district_hash        AS district,
+    date                       AS date,
+    time_slot                  AS time_slot,
     COUNT(start_district_hash) AS demand,
-    COUNT(driver_id)           AS answer,
-    date,
-    time_slot
-  FROM didi.`order`
+    COUNT(driver_id)           AS answer
+  FROM `order`
   GROUP BY start_district_hash, date, time_slot
   ORDER BY date ASC, time_slot ASC
 );
 
--- process poi features
-DROP TABLE IF EXISTS poi_index, poi_feature;
+CREATE INDEX order_stat_district_date_time_slot_index ON order_stat (district, date, time_slot)
 
-CREATE TABLE poi_index (
-  SELECT DISTINCT type
-  FROM poi
-);
-
-CREATE TABLE poi_feature (
-  SELECT
-    district.district_hash,
-    poi_index.type,
-    COALESCE(SUM(poi.count), 0) AS count
-  FROM district
-    CROSS JOIN poi_index
-    LEFT JOIN poi ON poi.district_hash = district.district_hash AND poi.type = poi_index.type
-  GROUP BY district_hash, type
-);
-
-DROP TABLE IF EXISTS poi_csv;
-
-CREATE TABLE poi_csv (
-  SELECT
-    district_hash,
-    GROUP_CONCAT(count ORDER BY type SEPARATOR ",") AS poi_feature
-  FROM poi_feature
-  GROUP BY district_hash);
-
-CREATE INDEX poi_feature_district_hash_index ON poi_feature (district_hash, type);
-CREATE INDEX poi_csv_district_hash_index ON poi_csv (district_hash);
-
--- create intermediate training set (without poi data)
-DROP TABLE IF EXISTS intermediate;
-
-CREATE TABLE intermediate (
-  SELECT
-    order_stat.district_hash              AS district,
-    order_stat.date                       AS date,
-    calendar.is_holiday                   AS is_holiday,
-    order_stat.time_slot                  AS time_slot,
-    COALESCE(weather.weather, 0)          AS weather,
-    COALESCE(weather.temperature, 0)      AS temperature,
-    COALESCE(weather.pm25, 0)             AS pm25,
-    COALESCE(traffic.level_1, 0)          AS level_1,
-    COALESCE(traffic.level_2, 0)          AS level_2,
-    COALESCE(traffic.level_3, 0)          AS level_3,
-    COALESCE(traffic.level_4, 0)          AS level_4,
-    order_stat.demand                     AS demand,
-    order_stat.answer                     AS answer,
-    order_stat.demand - order_stat.answer AS gap
+CREATE TABLE IF NOT EXISTS clock (
+  SELECT DISTINCT time_slot
   FROM order_stat
-    LEFT JOIN calendar ON calendar.date = order_stat.date
-    LEFT JOIN weather ON weather.date = order_stat.date AND weather.time_slot = order_stat.time_slot
-    LEFT JOIN traffic
-      ON traffic.district_hash = order_stat.district_hash
-         AND traffic.date = order_stat.date
-         AND traffic.time_slot = order_stat.time_slot
 );
+
+-- create feature set
+DROP TABLE IF EXISTS features;
+
+SELECT *
+FROM district
+  CROSS JOIN calendar
+  CROSS JOIN clock
+ORDER BY district_hash, date, time_slot;
+
+CREATE TABLE features (
+  SELECT
+    calendar.date                        AS date,
+    district.district_hash               AS district,
+    clock.time_slot                      AS time_slot,
+    COALESCE(calendar.is_holiday, FALSE) AS is_holiday,
+    COALESCE(weather.weather, 0)         AS weather,
+    COALESCE(weather.temperature, 0)     AS temperature,
+    COALESCE(weather.pm25, 0)            AS pm25,
+    COALESCE(traffic.level_1, 0)         AS level_1,
+    COALESCE(traffic.level_2, 0)         AS level_2,
+    COALESCE(traffic.level_3, 0)         AS level_3,
+    COALESCE(traffic.level_4, 0)         AS level_4
+  FROM district
+    CROSS JOIN clock
+    CROSS JOIN calendar
+    LEFT JOIN weather ON weather.date = calendar.date AND weather.time_slot = clock.time_slot
+    LEFT JOIN traffic
+      ON traffic.district_hash = district.district_hash
+         AND traffic.date = calendar.date
+         AND traffic.time_slot = clock.time_slot
+);
+
+CREATE INDEX features_date_time_slot_district_index ON didi.features (date, time_slot, district);
